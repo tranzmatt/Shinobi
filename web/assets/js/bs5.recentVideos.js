b554a5fc0901493b6128f8626a91f4077ce8dfae
Moe <github@m03.ca>
2021-10-26 17:21:53 -0700

    Monitor ID selector for Recent Videos window

--- web/assets/js/bs5.recentVideos.js
+++ web/assets/js/bs5.recentVideos.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var theBlock = $('#recentVideos')
     var theList = $('#recentVideosList')
+    var monitorList = theBlock.find('.monitors_list')
     function drawRowToList(row,toBegin){
         theList[toBegin ? 'prepend' : 'append'](createVideoRow(row))
     }
@@ -15,15 +16,36 @@ $(document).ready(function(){
             callback(data)
         })
     }
-    $('body').on('tab-open-initial',function(){
+    monitorList.change(function(){
+        var theSelected = `${monitorList.val()}`
         loadVideos({
             limit: 10,
+            monitorId: theSelected || undefined,
+        },function(){
+            liveStamp()
+        })
+    })
+    addOnTabReopen('initial', function () {
+        var theSelected = `${monitorList.val()}`
+        drawMonitorListToSelector(monitorList.find('optgroup'))
+        monitorList.val(theSelected)
+        loadVideos({
+            limit: 10,
+            monitorId: theSelected || undefined,
         },function(){
             liveStamp()
         })
     })
     onWebSocketEvent(function(d){
         switch(d.f){
+            case'init_success':
+                drawMonitorListToSelector(monitorList.find('optgroup'))
+                loadVideos({
+                    limit: 10,
+                },function(){
+                    liveStamp()
+                })
+            break;
             case'video_build_success':
                 loadVideoData(d)
                 drawRowToList(createVideoLinks(d),true)
