56ef5f6481b9df4845f14a3f0668c59e488173e0
Moe <github@m03.ca>
2022-05-02 16:23:29 -0700

    Update Language File Generator

--- tools/translateLanguageFile.js
+++ tools/translateLanguageFile.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 console.log('This translation tool uses a Google Translate scraper. Use responsibly or your IP will be blocked by Google from using the service.')
 if(!process.argv[2]||!process.argv[3]||!process.argv[4]){
     console.log('You must input arguments.')
@@ -13,62 +14,101 @@ try{
     console.log(`You are missing a module to use this tool. Run "npm install @vitalets/google-translate-api" to install the required module.`)
     return
 }
-var langDir='../languages/'
-var fs=require('fs');
-var https = require('https');
-var jsonfile=require('jsonfile');
-var source=require(langDir+process.argv[2]+'.json')
-var list = Object.keys(source)
+const langDir = `${__dirname}/../languages/`
+const sourceLangauge = process.argv[3]
+const inputFileLangauge = process.argv[2]
+const inputFileName = inputFileLangauge + '.json'
+const source = require(langDir + inputFileName)
+const list = Object.keys(source)
 console.log(list.length)
 var extra = ''
 var current = 1
 var currentItem = list[0]
-var chosenFile = langDir+process.argv[4]+'.json'
-var throttleTime = parseInt(process.argv[5]) || 1000
+const outputFileLangauge = process.argv[4]
+const outputFileName = outputFileLangauge + '.json'
+const chosenFile = langDir + outputFileName
+const generatedLanguageFilesPath = `${__dirname}/generatedLanguageFiles/`
+const generatedFilePath = `${generatedLanguageFilesPath}${outputFileName}`
+const throttleTime = parseInt(process.argv[5]) || 1000
+const usePendingFileForOutputSource = process.argv[6] === '1'
+let newList
 try{
-    newList=require(chosenFile)
+    const buildOutputSource = usePendingFileForOutputSource ? generatedFilePath : chosenFile
+    console.log(`Source Path : ${buildOutputSource}`)
+    newList = require(buildOutputSource)
 }catch(err){
-    console.log(chosenFile)
-    var newList={}
+    console.log(`There was an error loading : ${chosenFile}`)
+    console.log(`Using blank base file. This will translate against all available terms!!!`)
+    newList = {}
 }
-var newListAlphabetical={}
-var goNext=function(){
-    ++current
-    currentItem = list[current]
-    if(list.length===current){
-        console.log('complete checking.. please wait')
-            Object.keys(newList).sort().forEach(function(y,t){
-                newListAlphabetical[y]=newList[y]
-            })
-            jsonfile.writeFile(chosenFile,newListAlphabetical,{spaces: 2},function(){
-                console.log('complete writing')
-            })
-    }else{
+async function writeLanguageFile(theList,alternatePath){
+    const newListAlphabetical = {}
+    const sourceTermKeysOrdered = Object.keys(theList).sort()
+    sourceTermKeysOrdered.forEach(function(y){
+        newListAlphabetical[y] = theList[y]
+    })
+    await fs.promises.writeFile(alternatePath ? alternatePath : generatedFilePath,JSON.stringify(newListAlphabetical,null,3))
+}
+function asyncSetTimeout(timeout){
+    return new Promise((resolve,reject) => {
         setTimeout(() => {
-            next(currentItem)
-        },throttleTime)
+            resolve()
+        },timeout || 1000)
+    })
+}
+async function makeFolderForOutput(timeout){
+    try{
+        await fs.promises.mkdir(generatedLanguageFilesPath)
+    }catch(err){
+        console.log(err)
     }
 }
-var next=function(v){
-    if(v===undefined){return false}
-    if(newList[v]&&newList[v]!==source[v]){
-        goNext()
-        console.log('found a rule for this one, skipping : ',source[v]);
-        return
+async function moveNewLanguageFile(){
+    try{
+        await fs.promises.unlink(chosenFile)
+        await writeLanguageFile(newList,chosenFile)
+    }catch(err){
+        console.log('Failed to Move File!')
+        console.log(err)
     }
-    translate(source[v], {to: process.argv[4],from: process.argv[3]}).then(res => {
-        translation = res.text
-        newList[v] = translation;
-        console.log(current, ' of')
-        console.log(list.length)
-        console.log(v,' ---> ',translation)
-        goNext()
-    }).catch(err => {
-        translation = `${source[v]}`
-        console.log('translation failed : ',translation);
-        console.error(err);
-        newList[v]=translation;
-        goNext()
-    });
 }
-next(currentItem)
+function runTranslation(termKey,numberInLine){
+    return new Promise((resolve,reject) => {
+        if(termKey === undefined)return false;
+        const existingTerm = newList[termKey]
+        if(existingTerm && existingTerm !== source[termKey]){
+            resolve(existingTerm)
+            console.log('found a rule for this one, skipping : ',source[termKey]);
+            return
+        }
+        console.log(`${numberInLine} of ${list.length}`)
+        translate(source[termKey], {
+            to: outputFileLangauge,
+            from: sourceLangauge
+        }).then(res => {
+            translation = res.text;
+            newList[termKey] = translation;
+            console.log(termKey,' ---> ',translation)
+            setTimeout(() => {
+                resolve(translation)
+            },throttleTime)
+        }).catch(err => {
+            translation = `${source[termKey]}`
+            console.log('translation failed : ',translation);
+            console.error(err);
+            newList[termKey] = translation;
+            resolve()
+        });
+    })
+}
+async function runTranslatorOnSourceTerms(){
+    await makeFolderForOutput()
+    for (let i = 0; i < list.length; i++) {
+        let termKey = list[i]
+        await runTranslation(termKey,i)
+        await writeLanguageFile(newList)
+    }
+    await moveNewLanguageFile()
+    console.log('Building Language File Complete!')
+}
+runTranslatorOnSourceTerms()
