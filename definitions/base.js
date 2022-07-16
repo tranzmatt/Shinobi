7618c3083f5097bd713205ae4022a74e3843e465
Moe <github@m03.ca>
2022-07-16 09:30:17 -0700

    Add reconnect stream button to Live Grid block

--- definitions/base.js
+++ definitions/base.js
@@ -7322,6 +7322,7 @@ module.exports = function(s,config,lang){
                        <a title="${lang['Toggle Substream']}" class="btn btn-sm badge btn-secondary toggle-monitor-substream"><i class="fa fa-eye"></i></a>
                        <a title="${lang['Snapshot']}" class="btn btn-sm badge btn-warning snapshot-live-grid-monitor"><i class="fa fa-camera"></i></a>
                        <a title="${lang['Videos List']}" class="btn btn-sm badge btn-secondary open-videosTable"><i class="fa fa-film"></i></a>
+                       <a title="${lang['Reconnect Stream']}" class="btn btn-sm badge btn-success signal reconnect-live-grid-monitor"><i class="fa fa-plug"></i></a>
                        <a title="${lang['Show Logs']}" class="btn btn-sm badge btn-warning toggle-live-grid-monitor-logs"><i class="fa fa-exclamation-triangle"></i></a>
                        <a title="${lang['Close']}" class="btn btn-sm badge btn-danger close-live-grid-monitor"><i class="fa fa-times"></i></a>
                   </div>
