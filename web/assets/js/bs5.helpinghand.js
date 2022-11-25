23498eab73a06ddae23805536e11ce695929d068
Moe <github@m03.ca>
2022-11-24 16:53:10 -0800

    Update bs5.helpinghand.js

--- web/assets/js/bs5.helpinghand.js
+++ web/assets/js/bs5.helpinghand.js
@@ -1,7 +1,13 @@
 var helpingHand = null
+var isWatching = false
 function drawHelpingHand(){
     if(!helpingHand){
-        var html = `<div id="helping-hand"><i class="fa fa-3x fa-hand-pointer-o"></i></div>`
+        var html = `
+            <div id="helping-hand"><i class="fa fa-3x fa-hand-pointer-o"></i></div>
+            <div id="helping-hand-player">
+                <a class="btn btn-sm btn-danger helping-hand-stop" title="${lang.Stop}"><i class="fa fa-square"></i> ${lang.Stop}</a>
+            </div>
+        `
         $('body').append(html)
         helpingHand = $('#helping-hand')
     }
@@ -10,46 +16,51 @@ function removeHelpingHand(){
     if(helpingHand){
         helpingHand.fadeOut(2000)
         helpingHand.remove()
+        $('#helping-hand-player').remove()
         helpingHand = null
     }
 }
 async function typeWriteInField(txt,fieldTarget){
     var speed = 100;
-    var element = $(fieldTarget)[0]
+    var element = $(fieldTarget).focus().val('')[0]
     for (let i = 0; i < txt.length; i++) {
         element.value += txt.charAt(i);
         await setPromiseTimeout(speed);
     }
 }
+async function stopHelpingHandShow(showId){
+    isWatching = false
+}
 async function playHelpingHandShow(showId){
     drawHelpingHand()
+    isWatching = true
     var selectedShow = helpingHandShows[showId]
     var playlist = selectedShow.playlist
     for (let i = 0; i < playlist.length; i++) {
-        var movement = playlist[i];
-        var waitTime = movement.time * 1000
-        await setPromiseTimeout(waitTime);
-        var handPos = movement.handPos
-        var handPosCss = handPos
-        if(handPos.el){
-            var handElOffset = $(handPos.el).offset()
-            handElOffset.top += 30
-            handPosCss = handElOffset
-        }
-        var cmd = movement.cmd
-        helpingHand.css(handPosCss)
-        if(cmd){
-            await setPromiseTimeout(1500);
-            try{
-                await cmd();
-            }catch(err){
-                console.error(err)
+        if(isWatching){
+            var movement = playlist[i];
+            var waitTime = movement.time * 1000
+            await setPromiseTimeout(waitTime);
+            var handPos = movement.handPos
+            var handPosCss = handPos
+            if(handPos.el){
+                var handElOffset = $(handPos.el).offset()
+                handElOffset.top += 30
+                handPosCss = handElOffset
+            }
+            var cmd = movement.cmd
+            helpingHand.css(handPosCss)
+            if(cmd){
+                await setPromiseTimeout(1500);
+                try{
+                    if(isWatching)await cmd();
+                }catch(err){
+                    console.error(err)
+                }
             }
         }
     }
-    await setPromiseTimeout(3000);
+    if(isWatching)await setPromiseTimeout(3000);
     removeHelpingHand()
+    isWatching = false
 }
-$(document).ready(function(e){
-    playHelpingHandShow("motion-preset-pair")
-})
