624fb9e2eff3ed1a07fc389000400948ef7cc687
Moe <github@m03.ca>
2022-12-27 15:31:21 -0800

    Update bs5.fileBin.js

--- web/assets/js/bs5.fileBin.js
+++ web/assets/js/bs5.fileBin.js
@@ -1,3 +1,87 @@
+var loadedFilesInMemory = {}
+function getFileBinHref(file){
+    var href = getApiPrefix('fileBin') + '/' + file.mid + '/' + file.name
+    return href
+}
+function loadFileData(video){
+    delete(video.f)
+    loadedFilesInMemory[`${video.mid}${video.name}`] = Object.assign({},video,{
+        href: getFileBinHref(video),
+        filename: video.name,
+    })
+}
+function getFileBinFiles(monitorId,startDate,endDate){
+    return new Promise((resolve) => {
+        var apiURL = `${getApiPrefix('fileBin')}${monitorId ? `/${monitorId}` : ''}`;
+        var queryString = ['start=' + startDate,'end=' + endDate,'limit=0']
+        $.getJSON(apiURL + '?' + queryString.join('&'),function(data){
+            loadedFilesInMemory = {}
+            $.each(data.files,function(n,file){
+                loadFileData(file)
+            });
+            resolve(data)
+        })
+    })
+}
+function fileBinTableHeaderMap(headerColumns,headerConcat){
+    return headerColumns && headerConcat ? headerColumns : [
+          {
+              field: 'mid',
+              title: '',
+              checkbox: true,
+              formatter: () => {
+                  return {
+                      checked: false
+                  }
+              }
+          },
+          {
+            field: 'monitorName',
+            title: lang['Monitor']
+          },
+          {
+            field: 'name',
+            title: lang['Filename']
+          },
+          {
+            field: 'time',
+            title: lang['Time Created']
+          },
+          {
+            field: 'size',
+            title: ''
+          },
+          {
+            field: 'buttons',
+            title: ''
+          }
+    ].concat(headerColumns || []);
+}
+function fileBinTableMap(files,writeOptions){
+    return files.map((file) => {
+        var href = getApiPrefix('fileBin') + '/' + file.mid + '/' + file.name
+        var isVideo = file.name.includes('.mp4') || file.name.includes('.webm')
+        return Object.assign({
+            monitorName: `<b>${loadedMonitors[file.mid]?.name || file.mid}</b>`,
+            mid: file.mid,
+            name: file.name,
+            time: `
+                <div><b>${lang.Created}</b> ${formattedTime(file.time, 'DD-MM-YYYY hh:mm:ss AA')}</div>
+                ${file.details.start ? `<div><b>${lang.Started}</b> ${formattedTime(file.details.start, 'DD-MM-YYYY hh:mm:ss AA')}</div>` : ''}
+                ${file.details.end ? `<div><b>${lang.Ended}</b> ${formattedTime(file.details.end, 'DD-MM-YYYY hh:mm:ss AA')}</div>` : ''}
+            `,
+            size: convertKbToHumanSize(file.size),
+            buttons: `
+                <div class="row-info" data-mid="${file.mid}" data-ke="${file.ke}" data-time="${file.time}" data-name="${file.name}">
+                    <a class="btn btn-sm btn-primary" href="${href}" download title="${lang.Download}"><i class="fa fa-download"></i></a>
+                    ${isVideo ? `<a class="btn btn-sm btn-primary preview-video" href="${href}" title="${lang.Play}"><i class="fa fa-play"></i></a>` : ``}
+                    ${permissionCheck('video_delete',file.mid) ? `<a class="btn btn-sm btn-${file.archive === 1 ? `success status-archived` : `default`} archive-file" title="${lang.Archive}"><i class="fa fa-${file.archive === 1 ? `lock` : `unlock-alt`}"></i></a>` : ''}
+                    ${permissionCheck('video_delete',file.mid) ? `<a class="btn btn-sm btn-danger delete-file" title="${lang.Delete}"><i class="fa fa-trash-o"></i></a>` : ''}
+                </div>
+            `,
+        },writeOptions || {})
+    });
+}
 $(document).ready(function(e){
     var theEnclosure = $('#tab-fileBinView')
     var monitorsList = theEnclosure.find('.monitors_list')
@@ -5,13 +89,6 @@ $(document).ready(function(e){
     var fileBinDrawArea = $('#fileBin_draw_area')
     var fileBinPreviewArea = $('#fileBin_preview_area')
     var loadedFilesInMemory = {};
-    function getFileBinHref(file){
-        var href = getApiPrefix('fileBin') + '/' + file.mid + '/' + file.name
-        return href
-    }
-    function openFileBinView(monitorId,startDate,endDate){
-        drawFileBinViewElements(monitorId,startDate,endDate)
-    }
     loadDateRangePicker(dateSelector,{
         onChange: function(start, end, label) {
             drawFileBinViewElements()
@@ -20,86 +97,18 @@ $(document).ready(function(e){
     monitorsList.change(function(){
         drawFileBinViewElements()
     })
-    function loadFileData(video){
-        delete(video.f)
-        loadedFilesInMemory[`${video.mid}${video.name}`] = Object.assign({},video,{
-            href: getFileBinHref(video),
-            filename: video.name,
-        })
-    }
     function drawFileBinViewElements(selectedMonitor,startDate,endDate){
         var dateRange = getSelectedTime(dateSelector)
         if(!startDate)startDate = dateRange.startDate
         if(!endDate)endDate = dateRange.endDate
         if(!selectedMonitor)selectedMonitor = monitorsList.val()
-        var queryString = ['start=' + startDate,'end=' + endDate,'limit=0']
-        var frameIconsHtml = ''
-        var apiURL = getApiPrefix('fileBin') + '/' + selectedMonitor;
-        var fileBinData = []
-        loadedFilesInMemory = {}
-        $.getJSON(apiURL + '?' + queryString.join('&'),function(data){
-            $.each(data.files,function(n,file){
-                loadFileData(file)
-            })
+        getFileBinFiles(selectedMonitor,startDate,endDate).then((data) => {
             fileBinDrawArea.bootstrapTable('destroy')
             fileBinDrawArea.bootstrapTable({
                 pagination: true,
                 search: true,
-                columns: [
-                    {
-                      field: 'mid',
-                      title: '',
-                      checkbox: true,
-                      formatter: () => {
-                          return {
-                              checked: false
-                          }
-                      },
-                    },
-                      {
-                        field: 'monitorName',
-                        title: lang['Monitor']
-                      },
-                      {
-                        field: 'name',
-                        title: lang['Filename']
-                      },
-                      {
-                        field: 'time',
-                        title: lang['Time Created']
-                      },
-                      {
-                        field: 'size',
-                        title: ''
-                      },
-                      {
-                        field: 'buttons',
-                        title: ''
-                      }
-                ],
-                data: data.files.map((file) => {
-                    var href = getApiPrefix('fileBin') + '/' + file.mid + '/' + file.name
-                    var isVideo = file.name.includes('.mp4') || file.name.includes('.webm')
-                    return {
-                        monitorName: `<b>${loadedMonitors[file.mid]?.name || file.mid}</b>`,
-                        mid: file.mid,
-                        name: file.name,
-                        time: `
-                            <div><b>${lang.Created}</b> ${formattedTime(file.time, 'DD-MM-YYYY hh:mm:ss AA')}</div>
-                            ${file.details.start ? `<div><b>${lang.Started}</b> ${formattedTime(file.details.start, 'DD-MM-YYYY hh:mm:ss AA')}</div>` : ''}
-                            ${file.details.end ? `<div><b>${lang.Ended}</b> ${formattedTime(file.details.end, 'DD-MM-YYYY hh:mm:ss AA')}</div>` : ''}
-                        `,
-                        size: convertKbToHumanSize(file.size),
-                        buttons: `
-                            <div class="row-info" data-mid="${file.mid}" data-ke="${file.ke}" data-time="${file.time}" data-name="${file.name}">
-                                <a class="btn btn-sm btn-primary" href="${href}" download title="${lang.Download}"><i class="fa fa-download"></i></a>
-                                ${isVideo ? `<a class="btn btn-sm btn-primary preview-video" href="${href}" title="${lang.Play}"><i class="fa fa-play"></i></a>` : ``}
-                                ${permissionCheck('video_delete',file.mid) ? `<a class="btn btn-sm btn-${file.archive === 1 ? `success status-archived` : `default`} archive-file" title="${lang.Archive}"><i class="fa fa-${file.archive === 1 ? `lock` : `unlock-alt`}"></i></a>` : ''}
-                                ${permissionCheck('video_delete',file.mid) ? `<a class="btn btn-sm btn-danger delete-file" title="${lang.Delete}"><i class="fa fa-trash-o"></i></a>` : ''}
-                            </div>
-                        `,
-                    }
-                })
+                columns: fileBinTableHeaderMap(),
+                data: fileBinTableMap(data.files)
             })
         })
     }
